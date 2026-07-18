c06b90b62c65606b9ed33b0b5fc8802910b1c669
Noor Fatima <noor2239064@gmail.com>
2026-07-18 21:46:12 +0500

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const dotenv = require('dotenv');
+dotenv.config();
+const cors = require('cors');
+app.use(cors());
+const port = process.env.PORT;
+
+app.get('/', (req,res)=>{
+    res.send(`The Homepage is running on port ${port}`);
+});
+
+app.all('/*path', (req,res)=>{
+    res.send('This path does not exist');
+})
+
+app.listen(port, ()=>{
+    console.log(`The application is running on ${port}`);
+});
+
